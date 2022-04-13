e95c6a5660ad3965a6489391380192f83526f53a
Riley Paul <rileypaul96@gmail.com>
2022-04-13 07:36:16 -0700

    create text

---
+++ text.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from point import Point
+from typing import List
+
+@dataclass
+class Text:
+  name: str
+  position: Point
+  height: float = None
+  width: float = None
+  just: str = None
+  style: str = "Standard"
+  rot: float = None
+  lspace: float = None
+
+  def ACAD(self) -> List[str]:
+    command = f"_.-mtext _non {self.position.x},{self.position.y}"
+    command += f" J {self.just}" if self.just else ""
+    command += f" S {self.style}" if self.style else ""
+    command += f" R {self.rot}" if self.rot else ""
+    command += f" LS {self.lspace}" if self.lspace else ""
+    command += f" H {self.height}" if self.height else ""
+    command += f"\nW {self.width}" if self.width else "\nW 0"
+    command += f" {self.name}\n"
+    return [command]
