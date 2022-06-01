0839feb72534d4f80c1a6487d63ce8c95b03c53a
Riley Paul <rileypaul96@gmail.com>
2022-06-01 15:17:43 -0600

    Place space points functions in their own sub-module

---
+++ helpers/space_objects/__init__.py
@@ -0,0 +1,2 @@
+from autocad.helpers.space_objects.space_points import space_points
+from autocad.helpers.space_objects.space_ranges import space_ranges
--- space_objects/__init__.py
+++
@@ -1,2 +0,0 @@
-from autocad.space_objects.space_points import space_points
-from autocad.space_objects.space_ranges import space_ranges
